7f21b11f4e9e8c774729c1a16df9f13439dfe0b0
Rotmistrz <filip.markiewicz96@gmail.com>
2017-02-18 18:49:55 +0100

    Revert "Fixing .gitignore"
    This reverts commit 538961e5084fcd7b2c0d1c3c70cdf12a97345687.

--- eagle-config.php
+++ eagle-config.php
@@ -4,10 +4,10 @@ session_start();
 
 define('ITEMS_TABLE', 'eagle_items');
 
-define('DB_HOST', 'sql8.netmark.pl');
-define('DB_NAME', 'filipmar_eaglecms');
-define('DB_USERNAME', 'filipmar');
-define('DB_PASSWORD', 'ditsoapb123');
+define('DB_HOST', 'host-name');
+define('DB_NAME', 'database-name');
+define('DB_USERNAME', 'user-name');
+define('DB_PASSWORD', 'password');
 
 define('TEMPLATES_DIR', 'templates');
 define('TEMPLATE_EXTENSION', 'tpl');
