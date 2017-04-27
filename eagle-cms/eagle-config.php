95fc365b232a26630114868fe8492525830700c4
Filip <filip.markiewicz96@gmail.com>
2017-04-28 00:53:27 +0200

    Corrections

--- eagle-cms/eagle-config.php
+++ eagle-cms/eagle-config.php
@@ -4,10 +4,10 @@ session_start();
 
 define('ITEMS_TABLE', 'eagle_items');
 
-define('DB_HOST', 'sql8.netmark.pl');
-define('DB_NAME', 'filipmar_eaglecms');
-define('DB_USERNAME', 'filipmar');
-define('DB_PASSWORD', 'ditsoapb123');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'dbname');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
 
 define('TEMPLATES_DIR', 'templates');
 define('TEMPLATE_EXTENSION', 'tpl');
