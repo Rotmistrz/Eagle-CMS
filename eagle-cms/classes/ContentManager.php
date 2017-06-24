f1f5dc753663dba20056c73ac4dc22a7deee8067
Filip <filip.markiewicz96@gmail.com>
2017-06-24 19:39:05 +0200

    Loading selected items finished

--- eagle-cms/classes/ContentManager.php
+++ eagle-cms/classes/ContentManager.php
@@ -31,30 +31,92 @@ class ContentManager {
 		return $this;
 	}
 
-	public function getAllItemsByType($id) {
+	public function getItemsByType($type, $after, $limit) {
 		$itemsFactory = new ItemsCollectionFactory($this->loadHiddenItems);
-		$items = $itemsFactory->load($id);
+		$itemsFactory->after = $after;
+		$itemsFactory->limit = $limit;
+
+		if($this->orderType == Order::DESC) {
+			$itemsFactory->setDescendingOrder();
+		}
+
+		$items = $itemsFactory->load($type);
 
 		return $this->twig->render($this->template, array('itemsCollection' => $items->getContentsByLanguage($this->lang)));
 	}
 
-	public function getAllItemsByParent($parent, $id) {
+	public function getAllItemsByType($type) {
 		$itemsFactory = new ItemsCollectionFactory($this->loadHiddenItems);
-		$items = $itemsFactory->loadByParent($parent, $id);
+
+		if($this->orderType == Order::DESC) {
+			$itemsFactory->setDescendingOrder();
+		}
+
+		$items = $itemsFactory->load($type);
+
+		return $this->twig->render($this->template, array('itemsCollection' => $items->getContentsByLanguage($this->lang)));
+	}
+
+	public function getItemsByParent($parent, $type, $after, $limit) {
+		$itemsFactory = new ItemsCollectionFactory($this->loadHiddenItems);
+		$itemsFactory->after = $after;
+		$itemsFactory->limit = $limit;
+
+		if($this->orderType == Order::DESC) {
+			$itemsFactory->setDescendingOrder();
+		}
+
+		$items = $itemsFactory->loadByParent($parent, $type);
+
+		return $this->twig->render($this->template, array('itemsCollection' => $items->getContentsByLanguage($this->lang)));
+	}
+
+	public function getAllItemsByParent($parent, $type) {
+		$itemsFactory = new ItemsCollectionFactory($this->loadHiddenItems);
+
+		if($this->orderType == Order::DESC) {
+			$itemsFactory->setDescendingOrder();
+		}
+
+		$items = $itemsFactory->loadByParent($parent, $type);
+
+		return $this->twig->render($this->template, array('itemsCollection' => $items->getContentsByLanguage($this->lang)));
+	}
+
+	public function getItemsByCategory($category, $after, $limit) {
+		$itemsFactory = new ItemsCollectionFactory($this->loadHiddenItems);
+		$itemsFactory->after = $after;
+		$itemsFactory->limit = $limit;
+
+		if($this->orderType == Order::DESC) {
+			$itemsFactory->setDescendingOrder();
+		}
+
+		$items = $itemsFactory->loadByCategory($category);
 
 		return $this->twig->render($this->template, array('itemsCollection' => $items->getContentsByLanguage($this->lang)));
 	}
 
 	public function getAllItemsByCategory($category) {
 		$itemsFactory = new ItemsCollectionFactory($this->loadHiddenItems);
+
+		if($this->orderType == Order::DESC) {
+			$itemsFactory->setDescendingOrder();
+		}
+
 		$items = $itemsFactory->loadByCategory($category);
 
 		return $this->twig->render($this->template, array('itemsCollection' => $items->getContentsByLanguage($this->lang)));
 	}
 
-	public function getAllCategoriesByType($id) {
+	public function getAllCategoriesByType($type) {
 		$categoriesFactory = new CategoriesCollectionFactory($this->loadHiddenItems);
-		$categories = $categoriesFactory->load($id);
+
+		if($this->orderType == Order::DESC) {
+			$itemsFactory->setDescendingOrder();
+		}
+		
+		$categories = $categoriesFactory->load($type);
 
 		return $this->twig->render($this->template, array('categoriesCollection' => $categories->getContentsByLanguage($this->lang)));
 	}
